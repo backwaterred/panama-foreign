8e0d0790c64c9126a3c59109780fbcd88788d527
Tyler Steele <tyler.steele@ibm.com>
2022-05-12 11:55:59 -0600

    add empty constructor to UpcallContext

--- src/hotspot/share/prims/universalUpcallHandler.cpp
+++ src/hotspot/share/prims/universalUpcallHandler.cpp
@@ -47,6 +47,7 @@ extern struct JavaVM_ main_vm;
 struct UpcallContext {
   Thread* attachedThread;
 
+  UpcallContext() {}
   ~UpcallContext() {
     if (attachedThread != NULL) {
       JavaVM_ *vm = (JavaVM *)(&main_vm);
